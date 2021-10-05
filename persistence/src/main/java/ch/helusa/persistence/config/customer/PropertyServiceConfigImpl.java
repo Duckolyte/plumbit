0d219480e024784b407ece88d09adffaf12e4b38
andygg <andreas.gebert1988@gmail.com>
2021-10-06 00:56:34 +0200

    Made PropertyServiceConfigImpl a bean.

--- persistence/src/main/java/ch/helusa/persistence/config/customer/PropertyServiceConfigImpl.java
+++ persistence/src/main/java/ch/helusa/persistence/config/customer/PropertyServiceConfigImpl.java
@@ -1,4 +1,7 @@
 package ch.helusa.persistence.config.customer;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PropertyServiceConfigImpl implements PropertyServiceConfig {
 }
