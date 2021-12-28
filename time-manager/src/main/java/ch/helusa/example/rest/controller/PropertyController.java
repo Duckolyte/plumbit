41fa138aa7a942b03e2c76f827f8d5ecbfe8815c
Duckolyte <example@example.com>
2021-12-28 19:23:54 +0100

    Added property resource route exception handling.

--- time-manager/src/main/java/ch/helusa/example/rest/controller/PropertyController.java
+++ time-manager/src/main/java/ch/helusa/example/rest/controller/PropertyController.java
@@ -2,8 +2,11 @@ package ch.helusa.example.rest.controller;
 
 import ch.helusa.persistence.model.customer.Property;
 import ch.helusa.persistence.service.customer.PropertyService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 @RestController
@@ -18,7 +21,14 @@ public class PropertyController {
     @GetMapping("/property/{id}")
     @ResponseBody
     public Property getPropertyById(@PathVariable long id) {
-        return propertyService.findPropertyById(id);
+        try {
+            return propertyService.findPropertyById(id);
+        }  catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    MessageFormat.format("Property with id {0} not found.", id),
+                    e);
+        }
     }
 
     @GetMapping("/property")
@@ -30,7 +40,14 @@ public class PropertyController {
     @PostMapping("/property")
     @ResponseBody
     public Property createProperty(@RequestBody Property property) {
-        return propertyService.saveProperty(property);
+        try {
+            return propertyService.saveProperty(property);
+        }  catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    MessageFormat.format("Property with id {0} could not be saved.", property.getId()),
+                    e);
+        }
     }
 
 }
